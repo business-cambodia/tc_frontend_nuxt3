efcb0a38c100d7777d1ca820a005b8d223294c93
NarithNR9 <narith.neymar.jr@gmail.com>
2023-07-19 13:46:53 +0700

    set up base project

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLoadingIndicator :duration="5000" color="#089cf4" />
+  <NuxtLayout />
+  <NuxtPage />
+</template>
+
+<script setup></script>
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const colors = require("tailwindcss/colors");
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      height: {
+        hero: "42rem",
+        popup_mobile: "24rem",
+        popup: "34rem",
+      },
+      maxHeight: {
+        trending: "80vh",
+      },
+      minHeight: {
+        trending: "80vh",
+      },
+      gridTemplateColumns: {
+        footer: "1fr 3fr",
+        articles: "2fr 5fr 2fr",
+        articles_lg: "2fr 1fr",
+        profile: "2fr 3fr",
+        profile_2xl: "1fr 3fr",
+      },
+    },
+    colors: {
+      ...colors,
+      primary: "#089cf4",
+    },
+  },
+  plugins: [],
+};
