e633fef2c2d9718d1a53164ab38e4bf1423082cb
soun Soratha <thakhmer855@gmail.com>
2024-02-06 14:58:46 +0700

    completed footer for responsive mobile

--- components/layouts/FooterDesktop.vue
+++ components/layouts/FooterDesktop.vue
@@ -1,39 +1,39 @@
 <template>
   <div class="bg-primary dark:bg-gray-800 pb-4 text-white">
-    <div class="flex flex-col items-center justify-center top-footer-background-img py-20">
+    <div
+      class="flex flex-col items-center justify-center top-footer-background-img py-20"
+    >
       <img src="/footer/logo2.png" alt="" class="h-44 w-44" />
-      <div class="text-3xl font-bold">Technology Cambodia</div>
-      <div class="text-xl font-extralight">ដើម្បីកម្ពុជាកាន់តែទំនើបជាងមុន</div>
+      <div class="text-4xl font-bold">Technology Cambodia</div>
+      <div class="text-2xl font-extralight">ដើម្បីកម្ពុជាកាន់តែទំនើបជាងមុន</div>
       <div class="flex mt-2 justify-center items-center text-3xl">
-        <i class="fab fa-facebook mx-4"></i>
-        <i class="fab fa-youtube mx-4"></i>
-        <i class="fab fa-tiktok mx-4"></i>
-        <i class="fab fa-telegram mx-4"></i>
+        <i class="fab text-4xl fa-facebook mx-4"></i>
+        <i class="fab text-4xl fa-youtube mx-4"></i>
+        <i class="fab text-4xl fa-tiktok mx-4"></i>
+        <i class="fab text-4xl fa-telegram mx-4"></i>
       </div>
     </div>
     <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 pt-10 gap-4">
-      <div class="flex justify-center">
-        <div>
-          <div class="font-semibold text-xl md:text-2xl">
+      <div class="flex justify-center ">
+        <div> 
+          <div class="font-semibold text-xl md:text-2xl " id="aboutus">
             ស្វែងយល់អំពីពួកយើង
           </div>
-            <ul
-              class="mt-3 md:mt-6 list-none font-thin cursor-pointer "
-            >
-              <li class="transform hover:underline">
-                <nuxt-link to="" class="mx-1 md:mx-2">About Us</nuxt-link>
-              </li>
-              <li class="transform hover:underline">
-                <nuxt-link class="mx-1 md:mx-2">Our Services</nuxt-link>
-              </li>
-            </ul>
+          <ul class="mobile-respon mt-3 md:mt-6   list-none font-thin cursor-pointer">
+            <li class="transform hover:underline">
+              <nuxt-link to="" class="mx-1 md:mx-2">About Us</nuxt-link>
+            </li>
+            <li class="transform hover:underline">
+              <nuxt-link class="mx-1 md:mx-2">Our Services</nuxt-link>
+            </li>
+          </ul>
         </div>
       </div>
       <div class="flex justify-center">
         <div>
-          <div class="font-semibold text-xl md:text-2xl">ផ្នែកដែលពេញនិយម</div>
-          <div class="flex sm:flex-row justify-between mt-3 md:mt-6">
-            <ul class="list-none font-thin cursor-pointer">
+          <div class="font-semibold text-xl md:text-2xl" id="population">ផ្នែកដែលពេញនិយម</div>
+          <div class="wrap-ul flex sm:flex-row  justify-between mt-3 md:mt-6">
+            <ul class="mobile-respons-population list-none font-thin cursor-pointer">
               <li class="transform">
                 <nuxt-link>អេឡិចត្រូនិក</nuxt-link>
               </li>
@@ -44,7 +44,7 @@
                 <nuxt-link>យានយន្ត</nuxt-link>
               </li>
             </ul>
-            <ul class="list-none font-thin cursor-pointer">
+            <ul class="mobile-respons-population list-none font-thin cursor-pointer">
               <li class="transform hover:underline">
                 <nuxt-link>អាជីវកម្ម</nuxt-link>
               </li>
@@ -58,16 +58,15 @@
           </div>
         </div>
       </div>
+
       <div class="flex justify-center">
         <div>
-          <div class="font-semibold text-center text-xl md:text-2xl">
+          <div class="font-semibold text-start max-[774px]:text-center text-xl md:text-2xl" id="contactus">
             ទំនាក់ទំនង​មក​ពួក​យើង
           </div>
-          <ul
-            class="mt-3 md:mt-6 list-none font-thin cursor-pointer text-center"
-          >
-            <li class="transform hover:underline">
-              <a href="" target="_blank">
+          <ul class="mt-3 md:mt-6 list-none font-thin max-[774px]:text-center">
+            <li class="transform hover:underline cursor-pointer">
+              <a href="https://goo.gl/maps/GXV3J6cyRgoM9rGL6" target="_blank">
                 Address: Building 422 St. 271, Phum 1, <br />
                 Stung Mean Chey Khan MeanChey, Phnom Penh
               </a>
@@ -78,14 +77,12 @@
         </div>
       </div>
     </div>
-    
-    <div class="flex flex-col items-center mt-8">
+    <div class="flex flex-col  items-center mt-8 ">
       <div>© 2022 Baksey Media. All Rights Reserved.</div>
       <a
         href="https://www.facebook.com/p.both"
         class="transform hover:underline"
-        target="_blank"
-        >Developed by: Neak Panhboth</a
+        target="_blank">Developed by: Neak Panhboth</a
       >
     </div>
   </div>
@@ -95,11 +92,42 @@ export default {};
 </script>
 
 <style>
+/* image stye for top-footer-background-img  */
 .top-footer-background-img {
   background-image: url("/footer/foot_bg.jpg");
-  background-attachment: fixed;
   background-position: center;
-  background-repeat: no-repeat;
   background-size: cover;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  filter: brightness(1);
+
+}
+/* mobile responsive */
+@media (max-width: 774px) {
+  .mobile-respon {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  #aboutus{
+    font-size: 1.7rem;
+  }
+  #population{
+    font-size: 1.7rem;
+    text-align: center;
+  }
+  .mobile-respons-population{
+    display: flex;
+   padding: 10px 20px 0px 20px;
+  }
+  .wrap-ul{
+    display: flex;
+    flex-direction: column;
+  }
+  .mobile-respons-population li{
+    margin-left: 20px;
+  }
+  #contactus{
+    font-size: 1.7rem;
+  }
 }
 </style>
