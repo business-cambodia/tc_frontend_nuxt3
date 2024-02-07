40db38ccbf09caf9af0a1d5a9e4dfc69d1328a40
soun Soratha <thakhmer855@gmail.com>
2024-02-07 15:56:20 +0700

    update search not found

--- pages/search.vue
+++ pages/search.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- search page -->
   <div>
-    <div class="flex justify-center items-center h-96">
+    <div class="flex justify-center items-center h-[30rem]">
       <div class="flex flex-col items-center">
         <img src="/footer/logo2.png" class="w-20 h-20" alt="Website Logo" />
         <h1 class="text-blue-500 text-2xl font-bold mt-4">ស្វែងរកអត្ថបទ</h1>
@@ -26,21 +26,32 @@
             </button>
           </form>
         </div>
-       
+
         <!-- Error Message for No Articles Found -->
-        <div v-if="!searchFound" class="text-red-500">
-          មិនមានអត្ថបទត្រូវនឹងការស្វែងរករបស់អ្នក។
+        <div
+          v-if="!searchFound"
+          class="text-red-500 flex flex-col items-center justify-center mt-10"
+        >
+          <h2 class="text-4xl font-bold">
+            មិនមានអត្ថបទត្រូវនឹងការស្វែងរករបស់អ្នក <i class="fas fa-search mx-1" aria-hidden="true"></i>
+          </h2>
+          <a
+            href="/"
+            class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded"
+          >
+            ត្រឡប់ទៅទំព័រដើម
+          </a>
         </div>
       </div>
     </div>
     <div
-        class="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 justify-center gap-x-4 gap-y-8 mt-8 lg:px-20"
-        v-if="articles != undefined && articles.length > 0"
-      >
-        <div v-for="(a, n) in articles" :key="n" class="flex justify-center">
-          <ReusablesArticleCard :article="a" />
-        </div>
+      class="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 justify-center gap-x-4 gap-y-8 mt-8 lg:px-20"
+      v-if="articles != undefined && articles.length > 0"
+    >
+      <div v-for="(a, n) in articles" :key="n" class="flex justify-center">
+        <ReusablesArticleCard :article="a" />
       </div>
+    </div>
   </div>
 </template>
   <script setup lang="ts">
@@ -58,21 +69,21 @@ const search = ref("");
 const articles = ref<IArticle[]>([]);
 
 const performSearch = async () => {
-    searchFound = true;
-    requesting = false;
-    const response = (
-        await (<Promise<IResponse<IArticle[]>>>(
-            useApi(
-                `/items/articles?filter[status]=published&sort=-date_created&limit=10&page=1&search=${search.value}&fields=title, thumbnail, date_created,user_created.first_name, user_created.last_name, user_created.avatar,slug, category.slug, category.name,views`,
-                { method: "GET" }
-            )
-        ))
-    ).data;
-    // Check condition if not articles found
-    if(response.length === 0){
-        searchFound = false;
-    }
-    articles.value = [...response];
+  searchFound = true;
+  requesting = false;
+  const response = (
+    await (<Promise<IResponse<IArticle[]>>>(
+      useApi(
+        `/items/articles?filter[status]=published&sort=-date_created&limit=10&page=1&search=${search.value}&fields=title, thumbnail, date_created,user_created.first_name, user_created.last_name, user_created.avatar,slug, category.slug, category.name,views`,
+        { method: "GET" }
+      )
+    ))
+  ).data;
+  // Check condition if not articles found
+  if (response.length === 0) {
+    searchFound = false;
+  }
+  articles.value = [...response];
 };
 const handleScrollPagination = async () => {
   let documentHeight = document.body.scrollHeight;
