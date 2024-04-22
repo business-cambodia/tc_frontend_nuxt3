e40b81178803b8ab225329d412db121c51d86440
NarithNR9 <narith.neymar.jr@gmail.com>
2024-04-22 16:10:06 +0700

    [fix] drawer navbar wrong items display order

--- components/layouts/navbar.vue
+++ components/layouts/navbar.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
-    <nav id="navbar" :class="scrollPosition == 0
-      ? 'w-full flex items-center justify-between px-4 lg:px-8 py-2 fixed z-50 navbar'
-      : 'w-full flex items-center justify-between px-4 lg:px-8 py-2 fixed z-50 bg-white dark:bg-dark filter drop-shadow-lg navbar'
-      ">
-      <div class="flex items-center p-0.5 rounded-full hover:ring-1 hover:ring-primary">
+    <nav
+      id="navbar"
+      :class="
+        scrollPosition == 0
+          ? 'w-full flex items-center justify-between px-4 lg:px-8 py-2 fixed z-50 navbar'
+          : 'w-full flex items-center justify-between px-4 lg:px-8 py-2 fixed z-50 bg-white dark:bg-dark filter drop-shadow-lg navbar'
+      "
+    >
+      <div
+        class="flex items-center p-0.5 rounded-full hover:ring-1 hover:ring-primary"
+      >
         <NuxtLink to="/">
           <div class=""><img src="/logo.png" alt="" width="60" /></div>
         </NuxtLink>
       </div>
       <div>
         <ul class="hidden lg:flex list-none">
-          <NuxtLink v-for="(i, n) in navItems" :key="n" :to="i.slug ? `/category/${i.slug}` : '/'" :class="`mx-4 hover:text-primary nav-items text-base ${scrollPosition == 0 &&
-            ($route.name == 'index' ||
-              $route.name == 'articles-article' ||
-              $route.name == 'videos-video' ||
-              $route.name == 'videos' ||
-              $route.name == 'category-slug' ||
-              $route.name == 'authors-author')
-            ? 'text-white '
-            : 'text-black dark:text-white'
-            }`" exact-active-class="text-primary dark:text-primary font-semibold " exact>
+          <NuxtLink
+            v-for="(i, n) in navItems"
+            :key="n"
+            :to="i.slug ? `/category/${i.slug}` : '/'"
+            :class="`mx-4 hover:text-primary nav-items text-base ${
+              scrollPosition == 0 &&
+              ($route.name == 'index' ||
+                $route.name == 'articles-article' ||
+                $route.name == 'videos-video' ||
+                $route.name == 'videos' ||
+                $route.name == 'category-slug' ||
+                $route.name == 'authors-author')
+                ? 'text-white '
+                : 'text-black dark:text-white'
+            }`"
+            exact-active-class="text-primary dark:text-primary font-semibold "
+            exact
+          >
             <li>{{ i.name }}</li>
           </NuxtLink>
           <NuxtLink
@@ -63,53 +77,85 @@
         </form> -->
         <div class="mr-4">
           <NuxtLink to="/search">
-            <IconsSearch class="cursor-pointer" :scrollPosition="scrollPosition" />
+            <IconsSearch
+              class="cursor-pointer"
+              :scrollPosition="scrollPosition"
+            />
           </NuxtLink>
         </div>
 
         <div class="mr-6 cursor-pointer">
-          <button @click="
-            $colorMode.preference =
-            $colorMode.preference == 'dark' ? 'light' : 'dark'
-            " v-if="$colorMode.preference == 'dark'">
+          <button
+            @click="
+              $colorMode.preference =
+                $colorMode.preference == 'dark' ? 'light' : 'dark'
+            "
+            v-if="$colorMode.preference == 'dark'"
+          >
             <IconsMoon :scrollPosition="scrollPosition" />
           </button>
-          <button @click="
-            $colorMode.preference =
-            $colorMode.preference == 'dark' ? 'light' : 'dark'
-            " v-else>
+          <button
+            @click="
+              $colorMode.preference =
+                $colorMode.preference == 'dark' ? 'light' : 'dark'
+            "
+            v-else
+          >
             <IconsSun :scrollPosition="scrollPosition" />
           </button>
         </div>
 
         <div>
-          <IconsMenu class="cursor-pointer block lg:hidden" :toggleDrawer="toggleDrawer"
-            :scrollPosition="scrollPosition" />
+          <IconsMenu
+            class="cursor-pointer block lg:hidden"
+            :toggleDrawer="toggleDrawer"
+            :scrollPosition="scrollPosition"
+          />
         </div>
       </div>
     </nav>
 
-    <div class="h-screen w-full bg-primary dark:bg-gray-900 text-center fixed z-20 lg:hidden" v-if="drawer"
-      data-aos="fade-left">
-      <ul class="">
-        <div v-for="(i, n) in navItems" :key="n" @click="drawer = false" class="text-center">
-          <NuxtLink :to="`/category/${i.slug}`" class="mx-4 text-white list-none"
-            exact-active-class="text-white font-bold text-lg" exact @click="closeDrawer">
-            <li class=" hover-nav">{{ i.name }}</li>
+    <div
+      class="h-screen w-full bg-primary dark:bg-gray-900 text-center fixed z-20 lg:hidden"
+      v-if="drawer"
+      data-aos="fade-left"
+    >
+      <ul class="h-screen flex flex-col justify-center">
+        <div
+          v-for="(i, n) in navItems"
+          :key="n"
+          @click="drawer = false"
+          class="text-center h-12"
+        >
+          <NuxtLink
+            :to="`/category/${i.slug}`"
+            class="mx-4 text-white list-none"
+            exact-active-class="text-white font-bold text-sm"
+            exact
+            @click="closeDrawer"
+          >
+            <li class="hover-nav">{{ i.name }}</li>
           </NuxtLink>
         </div>
-        <NuxtLink to="/videos" class="mx-4 text-white list-none"
-            exact-active-class="text-white font-bold text-lg" exact @click="closeDrawer">
-            <li class=" hover-nav">វិដេអូ</li>
+        <div class="">
+          <NuxtLink
+            to="/videos"
+            class="mx-4 text-white list-none"
+            exact-active-class="text-white font-bold text-lg"
+            exact
+            @click="closeDrawer"
+          >
+            <li class="hover-nav">វិដេអូ</li>
           </NuxtLink>
+        </div>
       </ul>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { IResponse } from "types/api";
-import { ICategory } from "types/category";
+import { IResponse } from 'types/api';
+import { ICategory } from 'types/category';
 const scrollPosition = ref(0);
 const lastScroll = ref(0);
 const drawer = ref(false);
@@ -117,16 +163,16 @@ const search = ref(false);
 const navItems: ICategory[] = (
   await (<Promise<IResponse<ICategory[]>>>(
     useApi(
-      "/items/categories?fields=slug,id,name&sort=id&filter[status]=published",
-      { method: "GET" }
+      '/items/categories?fields=slug,id,name&sort=id&filter[status]=published',
+      { method: 'GET' }
     )
   ))
 ).data;
 
 const home: ICategory = {
-  name: "ទំព័រដើម",
-  slug: "",
-  thumbnail: "",
+  name: 'ទំព័រដើម',
+  slug: '',
+  thumbnail: '',
   articles: undefined,
 };
 
@@ -134,31 +180,34 @@ navItems.unshift(home);
 
 const toggleDrawer = () => {
   drawer.value = !drawer.value;
+  if (drawer.value) document.body.style.overflow = 'hidden';
+  else document.body.style.overflow = 'visible';
 };
 
 const closeDrawer = () => {
   drawer.value = false;
+  document.body.style.overflow = 'visible';
 };
 
 const updateScroll = () => {
   scrollPosition.value = window.scrollY;
   let scrollTop = window.scrollY || document.documentElement.scrollTop;
-  const navbar = document.getElementById("navbar")!;
+  const navbar = document.getElementById('navbar')!;
   if (scrollPosition.value == 0) {
-    navbar.style.top = "0";
+    navbar.style.top = '0';
   }
   if (scrollPosition.value > 0) {
     if (scrollTop > lastScroll.value) {
-      navbar.style.top = "-50%";
+      navbar.style.top = '-50%';
     } else {
-      navbar.style.top = "0";
+      navbar.style.top = '0';
     }
   }
   lastScroll.value = scrollTop;
 };
 
 onMounted(() => {
-  window.addEventListener("scroll", updateScroll);
+  window.addEventListener('scroll', updateScroll);
 });
 </script>
 
@@ -172,7 +221,7 @@ onMounted(() => {
 }
 
 .nav-items::after {
-  content: "";
+  content: '';
   position: absolute;
   background: #089cf4;
   height: 3px;
