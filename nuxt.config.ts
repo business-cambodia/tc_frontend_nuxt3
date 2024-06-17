18fece9ef36b8cd92539f32d2651b556e649b3e8
NarithNR9 <narith.neymar.jr@gmail.com>
2024-06-17 08:30:57 +0700

    [change] default color mode to light

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -126,7 +126,7 @@ export default defineNuxtConfig({
   },
   modules: ['@nuxtjs/color-mode', '@vite-pwa/nuxt'],
   colorMode: {
-    preference: 'system', // default value of $colorMode.preference
+    preference: 'light', // default value of $colorMode.preference
     fallback: 'light', // fallback value if not system preference found
     hid: 'nuxt-color-mode-script',
     globalName: '__NUXT_COLOR_MODE__',
