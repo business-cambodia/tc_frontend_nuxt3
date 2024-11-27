693e888b5fe3d6758fbe285a9a18a251253a4ef0
NarithNR9 <narith.neymar.jr@gmail.com>
2024-11-27 11:17:32 +0700

    [add] gpas underlay scripts

--- components/Articles/Content.vue
+++ components/Articles/Content.vue
@@ -89,6 +89,7 @@
             </div>
             <!--Mobile Underlay - Zone 1-->
             <div id="gax-inpage-async-1700709319"></div>
+            <ins data-revive-zoneid="125" data-revive-id="2d10743d9880200bf17a894cfa35dba0"></ins>
           </div>
 
           <div id="part-2">
