3ffb69d3479a12a123457b4d98df992887fa6997
ZensYing <thakhmer855@gmail.com>
2026-03-18 09:45:26 +0700

    fix gpas zone

--- components/Articles/Content.vue
+++ components/Articles/Content.vue
@@ -105,13 +105,7 @@
             <!-- Angkor Underlay Feb-2026 V2 - TC (Moved to top for better loading) -->
             <div class="underlay-ad underlay-ad--angkor">
               <ClientOnly>
-                <ins data-revive-zoneid="690" data-revive-id="2d10743d9880200bf17a894cfa35dba0"></ins>
-              </ClientOnly>
-            </div>
-            <!-- GPAS Mobile Underlay Angkor CNY Underlay Feb-2026 - TC- -->
-            <div class="underlay-ad underlay-ad--angkor">
-              <ClientOnly>
-                <ins data-revive-zoneid="673" data-revive-id="2d10743d9880200bf17a894cfa35dba0"></ins>
+                <ins data-revive-zoneid="705" data-revive-id="2d10743d9880200bf17a894cfa35dba0"></ins>
               </ClientOnly>
             </div>
             <!-- End Gpas Mobile Underlay -->
